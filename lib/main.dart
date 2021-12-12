6837bd4068d955c3c5b9b6d27e0c8add8e148118
excelitai-gunjon <gunjon@excelitai.com>
2021-12-12 17:59:07 +0600

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,10 @@ class MyApp extends StatelessWidget {
           // or simply save your changes to "hot reload" in a Flutter IDE).
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
         ),
-        //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+       // home: const PoductsOverviewScreen(),
       ),
     );
   }
